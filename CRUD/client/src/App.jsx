580d87aeb1cc669bb0211f8f7f182d89a4f7006b
waleed1857 <waleed.cool1857@gmail.com>
2025-12-20 18:44:30 +0500

    Work on Frontend

---
+++ CRUD/client/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+import Users from './Users'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />}></Route>
+          <Route path="/create" element={<CreateUser />}></Route>
+          <Route path="/update" element={<UpdateUser />}></Route>
+        </Routes>      
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
