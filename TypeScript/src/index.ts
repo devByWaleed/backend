0db0725a8a6744d006ce66f63e1e3e5221b6bc01
waleed1857 <waleed.cool1857@gmail.com>
2026-04-07 13:03:00 +0500

    Basic types

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,7 +1,64 @@
-// Plain Js
-// let id = 5
-
-// TypeScript
+// Basic types
 let id: number = 5;
+let company: string = "abc"
+let isApplied: boolean = false;
+let x: any = "Hello";
 
-console.log(id);
+// Array
+let nums: number[] = [1, 2, 3, 4];
+
+// Tuple
+let names: [string, boolean] = ["a", false];
+
+// Tuple Array
+let employee: [number, string][];
+employee = [
+    [1, "Emp1"],
+    [2, "Emp2"],
+]
+
+// console.log(employee);
+// console.log(typeof (employee));
+
+// Union
+let pid: string | number
+pid = "22"
+
+// enums
+enum Direction1 {
+    // Up = 1,
+    Up,
+    Down,
+    Left,
+    Right
+}
+// console.log(Direction1.Right);   // Up = 1, then should be 4
+// console.log(Direction1.Up);
+
+
+// Objects
+/*
+const user: {
+    id: number,
+    name: string
+} = {
+    id: 1,
+    name: "Waleed"
+}
+
+console.log(user);
+*/
+
+
+// 2nd option
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: "Waleed"
+}
+
+console.log(user);
